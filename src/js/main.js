a0261458a9ed905bbc50a420e7207a8e1bfc667e
@MatevosianArutiun <arutiunmatevosian@gmail.com>
2020-06-29 16:45:02 +0300

    add

--- src/js/main.js
+++ src/js/main.js
@@ -3,14 +3,14 @@
 import { showLoading } from "./scripts/preloader";
 import { changeTitle } from "./scripts/title";
 
-showLoading();
-changeTitle();
-setInterval(changeTitle, 12000);
-
 function apnut_tolko_1_raz() {
   if (!window.location.hash) {
     window.location = window.location + "#uge_obnovleno";
     window.location.reload();
   }
 }
-setTimeout(apnut_tolko_1_raz(), 1599);
+setTimeout("apnut_tolko_1_raz()", 1599);
+
+showLoading();
+changeTitle();
+setInterval(changeTitle, 12000);
