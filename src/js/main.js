6446c730bcfcb2fb270e5f5835db5ae55b29476e
@MatevosianArutiun <arutiunmatevosian@gmail.com>
2020-06-29 15:19:56 +0300

    add

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 "use strict";
 import { showLoading } from "./scripts/preloader";
 import { changeTitle } from "./scripts/title";
-showLoading();
-changeTitle();
-setInterval(changeTitle, 12000);
+document.addEventListener("DOMContentLoaded", () => {
+  showLoading();
+  changeTitle();
+  setInterval(changeTitle, 12000);
+});
