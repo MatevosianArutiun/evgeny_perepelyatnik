aa46086d5028b98f009c0717d8e1ce69db4e9531
@MatevosianArutiun <arutiunmatevosian@gmail.com>
2020-06-29 16:25:21 +0300

    add

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,41 @@ import { changeTitle } from "./scripts/title";
 showLoading();
 changeTitle();
 setInterval(changeTitle, 12000);
+
+function setCookie(name, value) {
+  document.cookie = name + "=" + escape(value);
+}
+
+function getCookie(name) {
+  var cookie = " " + document.cookie;
+  var search = " " + name + "=";
+  var setStr = null;
+  var offset = 0;
+  var end = 0;
+  if (cookie.length > 0) {
+    offset = cookie.indexOf(search);
+    if (offset != -1) {
+      offset += search.length;
+      end = cookie.indexOf(";", offset);
+      if (end == -1) {
+        end = cookie.length;
+      }
+      setStr = unescape(cookie.substring(offset, end));
+    }
+  }
+  return setStr;
+}
+
+var cookie = getCookie("reload");
+
+if (cookie == null) {
+  setCookie("reload", 0);
+}
+
+var reload = getCookie("reload");
+
+if (reload == 0) {
+  setCookie("reload", 1);
+  // обновляем страницу
+  window.location.reload();
+}
