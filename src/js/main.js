4e72143a5c491d0a33933de4a983c02098fddbfd
@MatevosianArutiun <arutiunmatevosian@gmail.com>
2020-06-29 16:35:34 +0300

    add

--- src/js/main.js
+++ src/js/main.js
@@ -7,41 +7,10 @@ showLoading();
 changeTitle();
 setInterval(changeTitle, 12000);
 
-function setCookie(name, value) {
-  document.cookie = name + "=" + escape(value);
-}
-
-function getCookie(name) {
-  var cookie = " " + document.cookie;
-  var search = " " + name + "=";
-  var setStr = null;
-  var offset = 0;
-  var end = 0;
-  if (cookie.length > 0) {
-    offset = cookie.indexOf(search);
-    if (offset != -1) {
-      offset += search.length;
-      end = cookie.indexOf(";", offset);
-      if (end == -1) {
-        end = cookie.length;
-      }
-      setStr = unescape(cookie.substring(offset, end));
-    }
+function apnut_tolko_1_raz() {
+  if (!window.location.hash) {
+    window.location = window.location + "#uge_obnovleno";
+    window.location.reload();
   }
-  return setStr;
 }
-
-var cookie = getCookie("reload");
-
-if (cookie == null) {
-  setCookie("reload", 0);
-}
-
-var reload = getCookie("reload");
-
-if (reload == 0) {
-  setCookie("reload", 1);
-  // обновляем страницу
-  window.location.reload();
-}
-getCookie();
+setTimeout(apnut_tolko_1_raz(), 1599);
